ee7008096c17cfce1a113f8e73084d27f221ba2e
jasofree <jasofree@gmail.com>
2025-10-17 19:32:01 +0200

    Cambio en la forma de mostrar los resultados

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -15,14 +15,16 @@ public final class App {
         int numero = 40;
         System.out.println();
         System.out.println();
-        long inicio = System.nanoTime();
-        System.out.printf("El término %s de la serie de Fibonacci es %s\n", numero, fibonacci_recursivo(numero));
-        long fin = System.nanoTime();
-        System.out.printf("La operación ha tardado %s nanosegundos\n", fin - inicio);
-        long inicio2 = System.nanoTime();
-        System.out.printf("El término %s de la serie de Fibonacci es %s\n", numero, fibonacci_formula(numero));
-        long fin2 = System.nanoTime();
-        System.out.printf("La operación ha tardado %s nanosegundos\n", fin2 - inicio2);
+        System.out.printf("Término %s\n", numero);
+        System.out.println("----------");
+        long inicio_fibonacci_recursivo = System.nanoTime();
+        int calculo_fibonacci_recursivo = fibonacci_recursivo(numero);
+        long fin_fibonacci_recursivo = System.nanoTime();
+        System.out.printf("Cálculo recursivo:\t %s - %s nanosegundos\n", calculo_fibonacci_recursivo, fin_fibonacci_recursivo - inicio_fibonacci_recursivo);
+        long inicio_fibonacci_formula = System.nanoTime();
+        int calculo_fibonacci_formula = fibonacci_formula(numero);
+        long fin_fibonacci_formula = System.nanoTime();
+        System.out.printf("Cálculo fórmula:\t %s - %s nanosegundos\n", calculo_fibonacci_formula, fin_fibonacci_formula - inicio_fibonacci_formula);
         System.out.println();
         System.out.println();
     }
