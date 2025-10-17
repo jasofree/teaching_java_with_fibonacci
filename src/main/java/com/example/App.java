40b1ad48137fbb1b5f291debfcc876a273983f51
jasofree <jasofree@gmail.com>
2025-10-17 17:08:12 +0200

    Función recursiva para Fibonacci

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -12,6 +12,19 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int numero = 6;
+        System.out.println();
+        System.out.println();
+        System.out.printf("El término %s de la serie de Fibonacci es %s\n", numero, fibonacci(numero));
+        System.out.println();
+        System.out.println();
+    }
+
+    static int fibonacci(int numero) {
+        if (numero <= 2) {
+            return numero - 1;
+        } else {
+            return fibonacci(numero - 1) + fibonacci(numero - 2);
+        }
     }
 }
